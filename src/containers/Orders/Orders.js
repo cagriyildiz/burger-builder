87a0ee30c32d8c9b50ccc004ef4e929f6021a3a1
cagriyildiz <cagriyild@gmail.com>
2019-03-25 21:57:56 +0300

    Output ingredients in the Order component

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -14,7 +14,6 @@ class Orders extends Component {
   componentDidMount() {
     axios.get('/orders.json')
       .then(res => {
-        console.log(res.data);
         let fetchedOrders = [];
         for (let key in res.data) {
           fetchedOrders.push({
@@ -32,8 +31,12 @@ class Orders extends Component {
   render() {
     return(
       <div>
-        <Order />
-        <Order />
+        {this.state.orders.map(order => (
+          <Order 
+            key={order.id}
+            ingredients={order.ingredients}
+            price={+order.price}/>
+        ))}
       </div>
     );
   }
