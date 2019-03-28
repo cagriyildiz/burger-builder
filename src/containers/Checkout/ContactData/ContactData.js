701c16405f4079d9777b1ea251d00cafd8acea6e
cagriyildiz <cagriyild@gmail.com>
2019-03-28 21:20:55 +0300

    Submit form data and store it in firebase

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -61,9 +61,15 @@ class ContactData extends Component {
   orderHandler = (event) => {
     event.preventDefault();
     this.setState({loading: true});
+    const formData = {};
+    for (let formElementIdentifier in this.state.orderForm) {
+      console.log(this.state.orderForm[formElementIdentifier]);
+      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+    }
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
+      orderData: formData
     };
     axios.post('/orders.json', order)
       .then(response => {
@@ -95,7 +101,7 @@ class ContactData extends Component {
     }
 
     let form = (
-      <form>
+      <form onSubmit={this.orderHandler}>
         {formElements.map(element => (
           <Input 
             key={element.id}
@@ -104,7 +110,7 @@ class ContactData extends Component {
             value={element.config.value}
             changed={(event) => this.inputChangedHandler(event, element.id)}/>
         ))}
-        <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+        <Button btnType="Success">ORDER</Button>
       </form>
     );
     if (this.state.loading) {
