a2ce72bc68c802992e1cd54a9a7bf5c03aa1898f
cagriyildiz <cagriyild@gmail.com>
2019-10-12 21:02:28 +0300

    Create action creators for login page

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.css';
+import * as actions from '../../store/actions/index';
 
 class Auth extends Component {
 
@@ -78,6 +80,11 @@ class Auth extends Component {
     this.setState({controls: updatedControls});
   };
 
+  submitHandler = (event) => {
+    event.preventDefault();
+    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+  }
+
   render() {
     const formElements = [];
     for (let key in this.state.controls) {
@@ -86,8 +93,6 @@ class Auth extends Component {
         config: this.state.controls[key]
       });
     }
-
-    console.log(formElements);
 
     const form = formElements.map(element => (
       <Input 
@@ -101,7 +106,7 @@ class Auth extends Component {
     ));
     return(
       <div className={classes.Auth}>
-        <form>
+        <form onSubmit={this.submitHandler}>
           {form}
           <Button btnType="Success">SUBMIT</Button>
         </form>
@@ -110,4 +115,10 @@ class Auth extends Component {
   };
 };
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+  return {
+    onAuth: (email, password) => dispatch(actions.auth(email, password))
+  };
+};
+
+export default connect(null, mapDispatchToProps)(Auth);
