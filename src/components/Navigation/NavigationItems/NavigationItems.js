6a9a281be26080ac1a64ea9c960b35f99a78ae8f
cagriyildiz <cagriyild@gmail.com>
2019-10-13 18:22:47 +0300

    Show relevant menu items for authentication

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,7 +6,9 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = (props) => (
   <ul className={classes.NavigationItems}>
     <NavigationItem link="/" exact clicked={props.clicked}>Burger Builder</NavigationItem>
-    <NavigationItem link="/orders" clicked={props.clicked}>Orders</NavigationItem>
+    { props.isAuthenticated ? 
+      <NavigationItem link="/orders" clicked={props.clicked}>Orders</NavigationItem> : null
+    }
     { props.isAuthenticated ? 
       <NavigationItem link="/logout" clicked={props.clicked}>Logout</NavigationItem> : 
       <NavigationItem link="/auth" clicked={props.clicked}>Login</NavigationItem>
