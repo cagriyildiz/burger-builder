e9d9d9e87cafb7cb93b50ac6b1fd2b1983af5600
cagriyildiz <cagriyild@gmail.com>
2019-10-13 15:49:13 +0300

    Send token if the user is authenticated

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -88,7 +88,7 @@ class ContactData extends Component {
       price: this.props.price,
       orderData: formData
     };
-    this.props.onOrderBurger(order);
+    this.props.onOrderBurger(order, this.props.token);
   }
 
   isValid(value, rules) {
@@ -173,13 +173,14 @@ const mapStateToProps = state => {
   return {
     ingredients: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
-    loading: state.order.loading
+    loading: state.order.loading,
+    token: state.auth.token
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onOrderBurger: (orderData) => {dispatch(actions.purchaseBurger(orderData))}
+    onOrderBurger: (orderData, token) => {dispatch(actions.purchaseBurger(orderData, token))}
   };
 };
 
