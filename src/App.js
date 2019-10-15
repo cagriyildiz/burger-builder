91d0f8c82239ab83766e45ff3833fd789395bb34
cagriyildiz <cagriyild@gmail.com>
2019-10-15 20:59:43 +0300

    Allow users to navigate to pages that he is authenticated to visit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
@@ -17,21 +17,39 @@ class App extends Component {
   };
 
   render() {
+    let route = (
+      <Switch>
+        <Route path='/' exact component={BurgerBuilder}/>
+        <Route path='/auth' component={Auth}/>
+        <Redirect to='/' />
+      </Switch>
+    );
+    if (this.props.isAuthenticated) {
+      route = (
+        <Switch>
+          <Route path='/' exact component={BurgerBuilder}/>
+          <Route path='/checkout' component={Checkout}/>
+          <Route path='/logout' component={Logout}/>
+          <Route path='/orders' component={Orders}/>
+          <Redirect to='/' />
+        </Switch>
+      );
+    }
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path='/' exact component={BurgerBuilder}/>
-            <Route path='/checkout' component={Checkout}/>
-            <Route path='/auth' component={Auth}/>
-            <Route path='/logout' component={Logout}/>
-            <Route path='/orders' component={Orders}/>
-          </Switch>
+          {route}
         </Layout>
       </div>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -39,4 +57,4 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default connect(null, mapDispatchToProps)(App);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
