bea1aa2832b45e4562eb68ced6fb2955ddb38bdc
cagriyildiz <cagriyild@gmail.com>
2019-10-15 22:11:56 +0300

    Lazy load some components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import asyncComponent from '../src/hoc/asyncComponent/asyncComponent';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
+
+const asyncCheckout = asyncComponent(() => {
+  return import('./containers/Checkout/Checkout');
+});
+
+const asyncOrders = asyncComponent(() => {
+  return import('./containers/Orders/Orders');
+});
+
+const asyncAuth = asyncComponent(() => {
+  return import('./containers/Auth/Auth');
+});
 
 class App extends Component {
 
@@ -20,7 +30,7 @@ class App extends Component {
     let route = (
       <Switch>
         <Route path='/' exact component={BurgerBuilder}/>
-        <Route path='/auth' component={Auth}/>
+        <Route path='/auth' component={asyncAuth}/>
         <Redirect to='/' />
       </Switch>
     );
@@ -28,10 +38,10 @@ class App extends Component {
       route = (
         <Switch>
           <Route path='/' exact component={BurgerBuilder}/>
-          <Route path='/checkout' component={Checkout}/>
-          <Route path='/auth' component={Auth}/>
+          <Route path='/checkout' component={asyncCheckout}/>
+          <Route path='/auth' component={asyncAuth}/>
           <Route path='/logout' component={Logout}/>
-          <Route path='/orders' component={Orders}/>
+          <Route path='/orders' component={asyncOrders}/>
           <Redirect to='/' />
         </Switch>
       );
