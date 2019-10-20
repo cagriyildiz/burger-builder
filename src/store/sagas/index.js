b190f90c4c2bdc3d3002fdb50601c01b4380db98
cagriyildiz <cagriyild@gmail.com>
2019-10-20 17:37:46 +0300

    Create sagas for orders

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -2,15 +2,21 @@ import { takeEvery } from 'redux-saga/effects';
 
 import { logoutSaga, checkAuthTimeoutSaga, authUserSaga, authCheckStateSaga } from './auth';
 import { initIngredientsSaga } from './burgerBuilder';
+import { purchaseBurgerSaga, fetchOrdersSaga } from './order';
 import * as actionTypes from '../actions/actionTypes';
 
-export function* watchAuth(action) {
+export function* watchAuth() {
   yield takeEvery(actionTypes.AUTH_INIT_LOGOUT, logoutSaga);
   yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
   yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
   yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga);
 }
 
-export function* watchBurgerBuilder(action) {
+export function* watchBurgerBuilder() {
   yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga);
+}
+
+export function* watchOrder() {
+  yield takeEvery(actionTypes.PURHCASE_BURGER, purchaseBurgerSaga);
+  yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga);
 }
